f86b849bd0adb2770573d8e899b9b6a4b2459d21
Suganthi Sivasankar <52537740+suganthi17@users.noreply.github.com>
2022-07-28 11:47:46 +0530

    Update CheckPasswrd.py

--- Login-Signup-with-Reset-password-using-otp/CheckPasswrd.py
+++ Login-Signup-with-Reset-password-using-otp/CheckPasswrd.py
@@ -1,6 +1,12 @@
+#import re --if not imported
+
 def chck_pS(pS,uS):
     while 1:
-        if len(pS)>=8 and pS.isalnum():
+        x1 = re.search("[a-zA-Z]", pS)
+        x2 = re.search("[0-9]",pS)
+        x3 = re.search("[@,#,$]",pS)
+        if x1 and x2 and x3 and len(pS)>=8:
+            print("valid password")
             d.update({uS:pS})              # d[uS] = pS
             return 'Successful'
             break
